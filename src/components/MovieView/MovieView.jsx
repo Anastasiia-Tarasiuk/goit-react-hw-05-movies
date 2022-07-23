cafcbfcce982daa8426857e3f5d0d050a4799811
Anastasiia-Tarasiuk <a.yu.tarasiuk@gmail.com>
2022-07-23 21:40:25 +0300

    Adds propTypes

--- src/components/MovieView/MovieView.jsx
+++ src/components/MovieView/MovieView.jsx
@@ -1,7 +1,8 @@
 import { Container, Poster, GenresList, GenresItem } from "./MovieView.styled";
+import PropTypes from 'prop-types'; 
 
 export const MovieView = ({ movieInfo }) => {
-
+  
     const { poster_path, title, vote_average, overview, genres } = movieInfo;
     const url = `https://image.tmdb.org/t/p/w300/${poster_path}`;
 
@@ -22,3 +23,15 @@ export const MovieView = ({ movieInfo }) => {
     )
 }
 
+MovieView.propTypes = {
+    onSubmit: PropTypes.shape({
+        poster_path: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        vote_average: PropTypes.number.isRequired,
+        overview: PropTypes.string.isRequired,
+        genres: PropTypes.arrayOf(PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            name: PropTypes.string.isRequired,
+        }))
+    })
+}
