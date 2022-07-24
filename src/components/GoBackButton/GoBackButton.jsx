d7e94d31464f5f060c56235187a67786035f7030
Anastasiia-Tarasiuk <a.yu.tarasiuk@gmail.com>
2022-07-24 10:21:17 +0300

    Adds GoBackButton navigation

--- src/components/GoBackButton/GoBackButton.jsx
+++ src/components/GoBackButton/GoBackButton.jsx
@@ -1,6 +1,13 @@
+import { useNavigate } from "react-router-dom";
 import { BackButton } from "./GoBackButton.styled"
 
 export const GoBackButton = () => {
+
+    const navigate = useNavigate();    
     
-    return <BackButton type="button">Go back</BackButton>
+    return (
+        <BackButton type="button" onClick={() => navigate(-1)}>
+            Go back
+        </BackButton>
+    )
 }
